d3fbe3f785891f111a24c76c1c9ca05f46e503ea
ousseynou8 <ousseynoufofana15@gmail.com>
2022-05-06 12:01:54 +0200

    mise a jour

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,15 @@
-import React from 'react'
-    import styled from "@emotion/styled"; 
-import { Text } from '@chakra-ui/react';
-import { TriangleDownIcon } from '@chakra-ui/icons';
- 
+import React from 'react';
+import {Text, Flex} from '@chakra-ui/react';
+import {TriangleDownIcon} from '@chakra-ui/icons';
+
 export const Footer = () => {
-
-
-
-    const CustomFooter = styled.div`
-  
-    background: wheat;
-    height: 10vh;
-    
-  `;
-
   return (
-    <CustomFooter>
-    <Text> <TriangleDownIcon/></Text>
-    <Text textAlign="center" fontSize={12} justifyContent="center">&copy;All right reserved 2022 .Inc </Text>
-    <Text textAlign="right" fontSize={12}>Réglements & autorisations</Text>
-</CustomFooter>
-  )
-}
+    <Flex bg="wheat" h="20vh">
+      <Text> <TriangleDownIcon /></Text>
+      <Text textAlign="center" fontSize={12} justifyContent="center">
+        &copy;All right reserved 2022 .Inc{' '}
+      </Text>
+      <Text textAlign="right" fontSize={12}>Réglements & autorisations</Text>
+    </Flex>
+  );
+};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,62 +1,82 @@
-import { Image, Text,  Button, Heading, Grid, GridItem } from "@chakra-ui/react";
+import {
+  Image,
+  Text,
+  Button,
+  Heading,
+  Grid,
+  GridItem, 
+  Box,
+  Badge
+} from "@chakra-ui/react";
 import eco from "../images/eco.png";
-  
+
 export const Home = () => {
   return (
+    <>
+      <Grid
+        display={["column", "column", "flex"]}
+        px={["1rem", "5rem", "5rem"]}
+        templateColumns="repeat(2, 1fr)"
+        gap={3}
+        alignItems="center"
+      >
+        <GridItem
+          display={"grid"}
+          alignContent="center"
+          justifyItems={"start"}
+          w="100%"
+          h="100%"
+          alignItems="center"
+        >
+          <Heading textTransform="uppercase">
+            La Technologie et l’Ecologie adaptées au Sénégal
+          </Heading>
+          <Text>
+            Oeuvrez à faire du Sénégal un pays d’exception et à créer des
+            solutions pour chaque besoin de nos concitoyens.
+          </Text>
+          <Button
+            colorScheme="whatsapp"
+            _hover={{ color: "white" }}
+            fontSize={["11", "12", "14", "16"]}
+          >
+            <Text>Agissez avec nous!</Text>
+          </Button>
+        </GridItem>
+        <GridItem w="100%" h="100%">
+          <Image src={eco} size="100%" />
+        </GridItem>
+      </Grid>
+
+      {/* ffff */}
+      <Grid templateColumns='repeat(2, 1fr)' gap={2}  px={"5rem"} py={"3rem"}  h="100vh">
+        
+     <Heading  align="center" >Notre Mission</Heading> 
+ 
+   <Box maxW={"sm"} borderWidth="1px" borderRadius='lg' overflow='hidden'>
+   <Box display='flex' alignItems='baseline'>
+   <Image src={eco} alt="" w="50%"/>
+          <Badge borderRadius='full' px='2' colorScheme='teal'>
+            New
+          </Badge>
+          <Box
+            color='gray.500'
+            fontWeight='semibold'
+            letterSpacing='wide'
+            fontSize='xs'
+            textTransform='uppercase'
+            ml='2'
+          >
+            dddd
+          </Box>
+        </Box>
+
+   </Box>
 
 
-<Grid 
-display={["column","column", "flex"]}
-px={["1rem","5rem","5rem"]}
-templateColumns='repeat(2, 1fr)' gap={3} alignItems="center">
-  <GridItem display={"grid"} alignContent="center" justifyItems={"start"} w='100%' h='100%'  alignItems="center">
-  <Heading textTransform="uppercase">
-          La Technologie et l’Ecologie adaptées au Sénégal
-        </Heading>
-        <Text>
-        Oeuvrez à faire du Sénégal un pays d’exception et à créer des
-          solutions pour chaque besoin de nos concitoyens.
-     </Text>
-        <Button
-          colorScheme="whatsapp"
-          _hover={{ color: "white" }}
-          fontSize={["11", "12", "14", "16"]}
-     >
-          <Text>Agissez avec nous!</Text>
-     </Button>
-  </ GridItem >
-  <GridItem w='100%' h='100%' >
-  <Image src={eco} size="100%" />
-  </ GridItem >
-</Grid>
-
-
-
-
-    // <HStack flexDirection="row-reverse" alignItems="center">
-    //   <Box>
-    //     <Image src={eco} width={["130px", "190px", "330px", "440px"]} />
-    //   </Box>
-
-    //   <Box 
-    //   as="div"
-    //   alignItems="flex-start">
-    //     <Heading textTransform="uppercase">
-    //       La Technologie et l’Ecologie adaptées au Sénégal
-    //     </Heading>
-    //     <Text>
-    //       Oeuvrez à faire du Sénégal un pays d’exception et à créer des
-    //       solutions pour chaque besoin de nos concitoyens.
-    //     </Text>
-    //     <Button
-    //       colorScheme="whatsapp"
-    //       _hover={{ color: "white" }}
-    //       fontSize={["11", "12", "14", "16"]}
-    //     >
-    //       <Text>Agissez avec nous!</Text>
-    //     </Button>
-    //   </Box>
-    // </HStack>
+ 
+      </Grid>
+    </>
   );
 };
 
